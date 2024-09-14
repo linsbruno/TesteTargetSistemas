1c0670af5538021686366d96743c8417afc4baf3
Bruno Lins <linslimabruno@gmail.com>
2024-09-13 22:10:00 -0300

    adicionando outros dois arquivos do desafio

---
+++ resultado.js
@@ -0,0 +1,12 @@
+let INDICE = 12;
+        let SOMA = 0;
+        let K = 1;
+
+        // Loop enquanto K < INDICE
+        while (K < INDICE) {
+            K = K + 1; // Incrementa K
+            SOMA = SOMA + K; // Atualiza SOMA com o valor de K
+        }
+
+        // Exibe o resultado
+        console.log(SOMA);
